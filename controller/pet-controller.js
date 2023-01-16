dbcbdbf77411bf3391cbe06cfa5327a1f1cf1fe6
Mofaz Shmuely <mofazshmuely69@gmail.com>
2023-01-16 13:37:22 +0200

    adding try & catch to the pet controller

--- controller/pet-controller.js
+++ controller/pet-controller.js
@@ -2,26 +2,27 @@ const petDal = require("../dal/pet-dal");
 const userDal = require("../dal/user-dal");
 
 function validatePetData(pet) {
-  if (pet.id) {
-    return "Cannot set pet id";
-  }
+  try {
+    if (pet.id) {
+      return "Cannot set pet id";
+    }
 
-  if (
-    !pet.name ||
-    !pet.type ||
-    !pet.color ||
-    !pet.height ||
-    !pet.weight ||
-    !pet.bio ||
-    // falsy = null, undefined, 0, '', false
-    // !undefined // true
-    // !false // true
-    pet.hypoallergenic === undefined || // true - valid, false - valid
-    !pet.dietaryRestrictions ||
-    !pet.breed ||
-    !pet.imageUrl
-  ) {
-    return "pet must have:name, type, color, height, weight, bio, hypoallergenic, dietaryRestrictions, breed";
+    if (
+      !pet.name ||
+      !pet.type ||
+      !pet.color ||
+      !pet.height ||
+      !pet.weight ||
+      !pet.bio ||
+      pet.hypoallergenic === undefined ||
+      !pet.dietaryRestrictions ||
+      !pet.breed ||
+      !pet.imageUrl
+    ) {
+      return "pet must have:name, type, color, height, weight, bio, hypoallergenic, dietaryRestrictions, breed";
+    }
+  } catch (err) {
+    res.status(500).send(err);
   }
 }
 
@@ -42,64 +43,96 @@ async function createNewPet(req, res) {
 }
 
 function updatePet(req, res) {
-  const petId = req.params.petId;
-  const petData = {
-    ...req.body,
-    hypoallergenic: req.body.hypoallergenic === "Yes",
-  };
+  try {
+    const petId = req.params.petId;
+    const petData = {
+      ...req.body,
+      hypoallergenic: req.body.hypoallergenic === "Yes",
+    };
 
-  // validate data
-  const validationResult = validatePetData(petData);
-  if (validationResult) {
-    return res.status(400).send(validationResult);
+    // validate data
+    const validationResult = validatePetData(petData);
+    if (validationResult) {
+      return res.status(400).send(validationResult);
+    }
+
+    petDal.updatePet(petId, petData);
+
+    res.json(petData);
+  } catch (err) {
+    res.status(500).send(err);
   }
-
-  petDal.updatePet(petId, petData);
-
-  res.json(petData);
 }
 
 function findPetById(req, res) {
-  const petId = req.params.petId; //1
-  const pet = [].find((pet) => pet.id === petId);
-  res.json(pet);
+  try {
+    const petId = req.params.petId; //1
+    const pet = [].find((pet) => pet.id === petId);
+    res.json(pet);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 async function getAllPets(req, res) {
-  const pets = await petDal.getAllPets();
-  res.json(pets);
+  try {
+    const pets = await petDal.getAllPets();
+    res.json(pets);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 async function adoptPet(req, res) {
-  const petId = req.params.petId;
-  const ownerId = req.user._id;
-  await petDal.adoptPet(petId, ownerId);
-  res.json({ message: "adopted successfully!" });
+  try {
+    const petId = req.params.petId;
+    const ownerId = req.user._id;
+    await petDal.adoptPet(petId, ownerId);
+    res.json({ message: "adopted successfully!" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 async function returnPet(req, res) {
-  const petId = req.params.petId;
-  await petDal.returnPet(petId);
-  res.json({ message: "returned pet successfully!" });
+  try {
+    const petId = req.params.petId;
+    await petDal.returnPet(petId);
+    res.json({ message: "returned pet successfully!" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 async function savePetToUser(req, res) {
-  const petId = req.params.petId;
-  const userId = req.user._id;
-  await userDal.savePetToUser(petId, userId);
-  res.json({ message: "Pet saved successfully!" });
+  try {
+    const petId = req.params.petId;
+    const userId = req.user._id;
+    await userDal.savePetToUser(petId, userId);
+    res.json({ message: "Pet saved successfully!" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 async function removePetFromUser(req, res) {
-  const petId = req.params.petId;
-  const userId = req.user._id;
-  await userDal.removePetFromUser(petId, userId);
-  res.json({ message: "Pet removed successfully!" });
+  try {
+    const petId = req.params.petId;
+    const userId = req.user._id;
+    await userDal.removePetFromUser(petId, userId);
+    res.json({ message: "Pet removed successfully!" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 async function findPetByUserId(req, res) {
-  const userId = req.params.userId;
-  const pets = await petDal.getPetByUserId(userId);
-  res.json(pets);
+  try {
+    const userId = req.params.userId;
+    const pets = await petDal.getPetByUserId(userId);
+    res.json(pets);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 module.exports = {
